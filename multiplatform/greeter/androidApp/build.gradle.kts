530f0ff24744d9ed722dd18c929a18414a86a666
Fred Porciúncula <tfcporciuncula@gmail.com>
2023-08-28 12:04:10 +0200

    Bump kotlin-inject

--- multiplatform/greeter/androidApp/build.gradle.kts
+++ multiplatform/greeter/androidApp/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 
 android {
   namespace = "com.fredporciuncula.inject.greeter.android"
-  compileSdk = 33
+  compileSdk = 34
   defaultConfig {
     applicationId = "com.fredporciuncula.inject.greeter.android"
     minSdk = 24
-    targetSdk = 33
+    targetSdk = 34
     versionCode = 1
     versionName = "1.0"
   }
