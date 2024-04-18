c96e1d1eb751279fa58f9ac60bd6166b0fee2d12
Eva Tatarka <eva@tatarka.me>
2024-04-17 21:01:22 -0700

    update android sample

--- android/ponyinject/app/build.gradle.kts
+++ android/ponyinject/app/build.gradle.kts
@@ -1,18 +1,18 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    id("com.google.devtools.ksp") version "1.9.10-1.0.13"
+    kotlin("android")
+    id("com.google.devtools.ksp")
     id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         namespace = "me.tatarka.inject.ponyinject"
         applicationId = "me.tatarka.inject.ponyinject"
         minSdk = 21
-        targetSdk = 31
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -22,7 +22,7 @@ android {
     }
 
     buildTypes {
-        get("release").apply {
+        release {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -37,7 +37,6 @@ android {
     }
     kotlinOptions {
         jvmTarget = "11"
-        freeCompilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
     }
     buildFeatures {
         viewBinding = true
@@ -46,23 +45,32 @@ android {
 
     sourceSets {
         // share code between unit and android tests
-        get("test").java {
-            srcDir("src/commonTest/java")
+        get("test").kotlin {
+            srcDir("src/commonTest/kotlin")
         }
-        get("androidTest").java {
-            srcDir("src/commonTest/java")
+        get("androidTest").kotlin {
+            srcDir("src/commonTest/kotlin")
         }
     }
     testOptions {
         unitTests.isReturnDefaultValues = true
+        managedDevices {
+            localDevices {
+                create("pixel2api33") {
+                    device = "Pixel 2"
+                    apiLevel = 33
+                    systemImageSource = "aosp"
+                }
+            }
+        }
     }
 }
 
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 
-    implementation("me.tatarka.inject:kotlin-inject-runtime:0.6.1")
-    ksp("me.tatarka.inject:kotlin-inject-compiler-ksp:0.6.1")
+    implementation("me.tatarka.inject:kotlin-inject-runtime:0.6.3")
+    ksp("me.tatarka.inject:kotlin-inject-compiler-ksp:0.6.3")
 
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     debugImplementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
@@ -73,27 +81,27 @@ dependencies {
 
     implementation("io.coil-kt:coil:2.2.2")
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.fragment:fragment-ktx:1.6.1")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.0")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.slidingpanelayout:slidingpanelayout:1.2.0")
-    debugImplementation("androidx.fragment:fragment-testing:1.6.1")
-    implementation("androidx.paging:paging-runtime-ktx:3.2.0")
+    debugImplementation("androidx.fragment:fragment-testing:1.6.2")
+    implementation("androidx.paging:paging-runtime-ktx:3.2.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("androidx.arch.core:core-testing:2.2.0")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
-    testImplementation("com.willowtreeapps.assertk:assertk:0.26.1")
+    testImplementation("com.willowtreeapps.assertk:assertk:0.28.0")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("com.squareup.okhttp3:mockwebserver:4.9.1")
     androidTestImplementation("com.jakewharton.espresso:okhttp3-idling-resource:1.0.0")
-    androidTestImplementation("com.willowtreeapps.assertk:assertk:0.26.1")
+    androidTestImplementation("com.willowtreeapps.assertk:assertk:0.28.0")
 }
--- android/ponyinject/build.gradle.kts
+++ android/ponyinject/build.gradle.kts
@@ -1,23 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.0.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.0")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle.kts files
-    }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
-}
-
 tasks.wrapper {
     jarFile = file("../../gradle/wrapper/gradle-wrapper.jar")
 }
